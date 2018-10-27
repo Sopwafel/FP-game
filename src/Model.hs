eefcab8dbe5103ea845569bc5dbf11ad4b1aa1d2
Delta <maxreitsma@live.nl>
2018-10-27 23:48:25 +0200

    useless work i guess, not salty about it all.... what are you talking about....

--- src/Model.hs
+++ src/Model.hs
@@ -21,14 +21,16 @@ data GameState = GameState {
 data Bullet = Bullet {location :: Point, damage :: Int, image :: Picture, speed :: Int}
 
 testBullet = Bullet {damage = 1, image = Circle 1.0, speed = 5}
+enemyBullet = Bullet {damage = 1, image = circle 1.0, speed = -3}
 
 data Enemy  = Enemy  {location :: Point, health :: Int, image :: Picture, speed :: Int, bullet :: Bullet} 
+testEnemy = Enemy {location = (600, 0), health = 5, image = Arc 130.0 230.0 20.0, speed = 0, bullet = enemyBullet}
 
 
 -- class Draw a where
     -- Draw :: a ->
     
 data Player = Player { image :: Picture, location :: Point}
-testPlayer = Player { image = Circle 10.0,  location = (0.0, 0.0)}
+testPlayer = Player { image = ThickCircle 1.0 20.0,  location = (-450.0, 0.0)}
 
-beginState = GameState { player = testPlayer, pressedKeys = [], enemies = [], friendlyBullets = [], enemyBullets = []}
+beginState = GameState { player = testPlayer, pressedKeys = [], enemies = [testEnemy], friendlyBullets = [], enemyBullets = []}
