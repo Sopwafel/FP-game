c01ca0e1241152871a72adcf2a333e7e06e0838f
Unknown <vavandenbroek@gmail.com>
2018-11-01 15:48:17 +0100

    Code rondschuiven

--- src/Model.hs
+++ src/Model.hs
@@ -23,64 +23,94 @@ data GameState = GameState {
 -- | Pictures!
 square = Polygon [(-2,-2),(2,-2),(2,2),(-2,2),(-2,-2)]
 
--- || Type Classes ###################################################################################################### | --
+-- || Type Classes and instances ######################################################################################### | --
 -- | Returns a picture of a drawable object
 class Draw d where
     draw            :: d -> Picture
+    
+instance Draw Bullet where
+    draw Bullet {image = img, location = (x,y)} = (translate x y img)
+instance Draw Enemy where
+    draw Enemy {image = img, location = (x,y)} = (translate x y img)
+    
 -- | Updates an object for 1 step
 class Update a where
     update          :: a -> Maybe a
-    
-class Collideable a where
-    loc             :: a -> Point
-    hitboxSize      :: a -> Int
 
-class Damage a where
-    damage          :: a -> Int
-    
-class DamageAble a where
-    doDamage        :: (Damage b) => a -> b -> a
-    explode         :: a -> Explosion
-    
--- |
-collides :: (Collideable a, Collideable b) => a -> b -> Bool
-collides a b = collideHelper (loc a) (loc b) (fromIntegral((hitboxSize a) `div` 2)) (fromIntegral((hitboxSize b) `div` 2))
-collideHelper :: Point -> Point -> Float -> Float -> Bool
-collideHelper (x,y) (x2,y2) s1 s2 = x < (x2+s2) && (x+s1) > x2 && y < (y2+s2) && (y+s1) > y2
-
-
--- || Game Data Types ################################################################################################### | --
-
--- | This works because of the DuplicateRecordFields extension
-data Bullet = Bullet {location :: Point, damagePoints :: Int, image :: Picture, path :: ObjectPath, size :: Int}
-instance Draw Bullet where
-    draw Bullet {image = img, location = (x,y)} = (translate x y img)
 instance Update Bullet where
-    update bullet@Bullet{location = (x,y), path = StraightPath v} = Just (bullet {location = (x+v, y)} :: Bullet)
-instance Collideable Bullet where
-    loc Bullet{location = a} = a
-    hitboxSize Bullet{size = a} = a
-instance Damage Bullet where
-    damage Bullet{damagePoints = d} = d
-    
-
-
-    
-data Enemy  = Enemy {location :: Point, health :: Int, image :: Picture, path :: ObjectPath, bullet :: Bullet, shotCooldown :: Int, shotCooldownCounter :: Int, size :: Int } 
-instance Draw Enemy where
-    draw Enemy {image = img, location = (x,y)} = (translate x y img)
+    update bullet@Bullet{location = (x,y), path = StraightPath v} = Just (bullet {location = (x+v, y)} :: Bullet)    
 instance Update Enemy where
     update enemy@Enemy{location   = (x,y), path = StraightPath v, shotCooldownCounter = shotCooldownCounter, shotCooldown = shotCooldown, health = h}
         | h < 0 = Nothing
         | shotCooldownCounter < shotCooldown = Just enemy  {location = (x+v, y), shotCooldownCounter = shotCooldownCounter +1} :: Maybe Enemy
         | otherwise = Just enemy  {location = (x+v, y), shotCooldownCounter = 0} :: Maybe Enemy
+instance Update Explosion where
+    update boom@Explosion{countdown = count, location = (x, y), velocity = (x2, y2)}
+        | count > 0 = Just boom {countdown = count - 1, location = ((x + x2), (y + y2))}
+        | otherwise = Nothing
+
+-- | An object that can collide with another object
+class Collideable a where
+    loc             :: a -> Point
+    hitboxSize      :: a -> Int
+
+instance Collideable Bullet where
+    loc Bullet{location = a} = a
+    hitboxSize Bullet{size = a} = a
 instance Collideable Enemy where
     loc Enemy{location = a} = a
     hitboxSize Enemy{size = a} = a
+instance Collideable Player where
+    loc Player{location = a} = a
+    hitboxSize Player{size = a} = a
+    
+-- | A thing that does damage
+class Damage a where
+    damage          :: a -> Int
+    
+instance Damage Bullet where
+    damage Bullet{damagePoints = d} = d
+-- | A thing that can take damage
+class DamageAble a where
+    doDamage        :: (Damage b) => a -> b -> a
+    explode         :: a -> Explosion
+    
 instance DamageAble Enemy where
     doDamage enemy@Enemy{health = h} bullet = enemy {health = (h-(damage bullet))}
     explode enemy@Enemy{location = location} = testExplosion {location = location }
-        
+instance DamageAble Player where
+    doDamage p@Player{health = h} bullet = p {health = (h-(damage bullet))}
+    explode player@Player{location = location} = testExplosion {location = location}
+    
+-- | 
+collides :: (Collideable a, Collideable b) => a -> b -> Bool
+collides a b = collideHelper (loc a) (loc b) (fromIntegral((hitboxSize a) `div` 2)) (fromIntegral((hitboxSize b) `div` 2))
+collideHelper :: Point -> Point -> Float -> Float -> Bool
+collideHelper (x,y) (x2,y2) s1 s2 = x < (x2+s2) && (x+s1) > x2 && y < (y2+s2) && (y+s1) > y2
+
+-- || Instances########################################################################################################## | --
+-- | Bullet
+
+
+
+
+    
+-- | Enemy
+
+
+
+
+-- | Player
+
+
+    
+-- || Game Data Types ################################################################################################### | --
+
+-- | This works because of the DuplicateRecordFields extension
+data Bullet = Bullet {location :: Point, damagePoints :: Int, image :: Picture, path :: ObjectPath, size :: Int}
+  
+data Enemy  = Enemy {location :: Point, health :: Int, image :: Picture, path :: ObjectPath, bullet :: Bullet, shotCooldown :: Int, shotCooldownCounter :: Int, size :: Int } 
+       
 enemyCanShoot :: Enemy -> Bool
 enemyCanShoot Enemy{shotCooldown = shotCooldown, shotCooldownCounter = shotCooldownCounter} = shotCooldown == shotCooldownCounter
 enemyShoots :: Enemy -> Bullet
@@ -92,18 +122,9 @@ data ObjectPath = StraightPath Float       -- Float is speed
     | HomingPath   Float Float Float       -- First Float is speed, second has range -1..1 and is the direction, third has range 0..1 and is turning rate
 
 data Player = Player { image :: Picture, location :: Point, bullet :: Bullet, shotCooldown :: Int, size :: Int, health :: Int}
-instance Collideable Player where
-    loc Player{location = a} = a
-    hitboxSize Player{size = a} = a
-instance DamageAble Player where
-    doDamage p@Player{health = h} bullet = p {health = (h-(damage bullet))}
-    explode player@Player{location = location} = testExplosion {location = location}
 
 data Explosion = Explosion {location :: Point, scale :: Float, countdown :: Int, velocity :: Point}
-instance Update Explosion where
-    update boom@Explosion{countdown = count, location = (x, y), velocity = (x2, y2)}
-        | count > 0 = Just boom {countdown = count - 1, location = ((x + x2), (y + y2))}
-        | otherwise = Nothing
+
     
 -- || Wave logic ######################################################################################################## | --
 
